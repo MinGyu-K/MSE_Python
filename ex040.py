3ba88b756f5125098b4da04a723eef26cc4d57e7
MinGyu-K <mandu0413@gmail.com>
2020-12-13 21:01:14 +0900

    added file

---
+++ ex040.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[13]:
+
+
+data = "  삼성전자  "
+print(data)
+# strip()을 사용하여 공백을 제거
+change = data.strip()
+print(change)
+
---
+++ ex050.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[14]:
+
+
+data = "039490  "
+print(data)
+# rstrip은 우측의 공백을 제거하기 때문에 rstrip()을 사용하여 제거
+change = data.rstrip()
+print(change)
+
