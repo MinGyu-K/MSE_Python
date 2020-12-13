80035ae5c88f1a1da9cc4d8df8f6785d72049494
MinGyu-K <mandu0413@gmail.com>
2020-12-13 20:58:54 +0900

    added file

---
+++ ex020.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+# money와 month라는 변수에 숫자를 넣음
+money = 48584
+month = 36
+# sum이라는 변수를 money와 month를 곱한 변수라고 지정
+sum = money * month
+print(sum)
+
